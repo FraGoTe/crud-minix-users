c67d31fae4cb4ead610dba4baca876599a31ffc2
Francis Gonzales <root@10.0.2.15>
2013-11-27 06:55:07 +0000

    Agregando el switch

--- crud.sh
+++ crud.sh
@@ -5,7 +5,7 @@ echo "3. Modificar usuario"
 echo "4. Creditos"
 echo "5. Salir"
 
-read varNumber
+read valOpcion
 
 #Validando que el dato ingresado sea un numero
 #regexNumber = '^[0-9]+$'
@@ -13,4 +13,42 @@ read varNumber
 #if ! [ $varNumber =~ $regexNumber ] ; then
 #   echo "Error el dato ingresado no es un numero" >&2; exit 1
 #fi
+case "$valOpcion" in
+	"1")
+	   crear_usuario()
+         ;;
+	"2")
+	   eliminar_usuario()
+	 ;;
+	"3")
+	   editar_usuario()
+	 ;;
+	"4")
+	   creditos()
+	 ;;
+	 *)
+	   exit 1
+	;;
+esac
 
+
+# Definiendo la funcion de crear usuario
+crear_usuario(){
+    echo "Crear usuario"
+    exit 1
+}
+# Definiendo la funcion de eliminar usuario
+eliminar_usuario(){
+    echo "Eliminar usuario"
+    exit 1
+}
+# Definiendo la funcion de modificar usuario
+modificar_usuario(){
+    echo "Modificar usuario"
+    exit 1
+}
+# Definiendo los creditos
+creditos(){
+    echo "Developed by FraGoTe"
+    exit 1
+}
