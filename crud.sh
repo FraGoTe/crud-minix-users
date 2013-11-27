60bc1bba60e7fcf90289753c0074d1ec270ab5a0
Big Brother <root@10.0.2.15>
2013-11-27 06:39:09 +0000

    dejando las validaciones para el final

--- crud.sh
+++ crud.sh
@@ -8,9 +8,9 @@ echo "5. Salir"
 read varNumber
 
 #Validando que el dato ingresado sea un numero
-regexNumber = '^[0-9]+$'
+#regexNumber = '^[0-9]+$'
 
-if ! [[ $varNumber =~ $regexNumber ]] ; then
-   echo "Error el dato ingresado no es un numero" >&2; exit 1
-fi
+#if ! [ $varNumber =~ $regexNumber ] ; then
+#   echo "Error el dato ingresado no es un numero" >&2; exit 1
+#fi
 
